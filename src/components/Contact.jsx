0ef8500c9fc85d29b5ebe80ae5a9f941c19edabf
mrdooby <mrdooby@DESKTOP-OC2PQIT.localdomain>
2022-08-22 18:25:02 -0700

    feat: contact layout donw

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,58 @@ import styled from 'styled-components';
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <ContactContainer>
+      <ContactBox>
+        <ContactTitle>Contact me</ContactTitle>
+        <ContactBody>reach me at ____</ContactBody>
+        <ContactButton>say hello</ContactButton>
+        <ContactText>email copied to clipboard</ContactText>
+      </ContactBox>
+    </ContactContainer>
   )
 }
 
 export default Contact;
+
+// styled components
+const ContactContainer = styled.section`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  border: solid red;
+`;
+
+const ContactBox = styled.div`
+  width: 70%;
+  border: solid white;
+  align-self: center;
+  text-align: center;
+`;
+
+const ContactTitle = styled.div`
+  font-size: 100px;
+  padding-left: 1%;
+  font-weight: bold;
+  color: #fffcf2;
+`;
+
+const ContactBody = styled.div`
+  color: #ccc5b9;
+`;
+
+const ContactButton = styled.button`
+`;
+
+const ContactText = styled.div`
+  color: #ccc5b9;
+`;
+// #fffcf2 floral white,
+//    pop out, headers and titles
+// #ccc5b9 pale silver,
+//    normal text
+// #403d39 black olive,
+//    box around text
+// #252422 eerie black,
+//    background
+// #eb5e28 flame
+//    hightlight around buttons
