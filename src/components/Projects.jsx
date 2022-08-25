5b5c623f11b72b4037293c0ee9ffe43942728b5c
mrdooby <mrdooby@DESKTOP-OC2PQIT.localdomain>
2022-08-24 23:21:08 -0700

    feat: added title to data, increased font size, shadow effect on button

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { projectData } from '../data/ProjectsData.js';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Projects = () => {
 
@@ -15,16 +16,39 @@ const Projects = () => {
         </ProjectsTitleBox>
         <GridContainer>
           <Grid container spacing={0}
-          border='solid pink'
           direction={{xs: 'row', sm: 'row', md: 'column'}}
           overflow='auto'
           >
-            <GridImage item border='solid red' position='relative' xs={12} sm={12} md={6} height={{xs: '50%'}} image={projectData[0].img}></GridImage>
-            <GridText item border='solid red' position='relative' xs={12} sm={12} md={6} height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>{projectData[0].info}</GridText>
-            <GridImage item border='solid blue' position='relative' xs={12} sm={12} md={6} height={{xs: '50%'}} image={projectData[1].img}></GridImage>
-            <GridText item border='solid blue' position='relative' xs={12} sm={12} md={6} height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>{projectData[1].info}</GridText>
-            <GridImage item border='solid yellow' position='relative' xs={12} sm={12} md={6} height={{xs: '50%'}} image={projectData[2].img}></GridImage>
-            <GridText item border='solid yellow' position='relative' xs={12} sm={12} md={6} height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>{projectData[2].info}</GridText>
+            <GridImage item
+            position='relative' xs={12} sm={12} md={6} height={{xs: '50%'}}
+            image={projectData[0].img} />
+            <GridText container item
+            position='relative' xs={12} sm={12} md={6}
+            height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>
+              <>{projectData[0].title}</><br></br>
+              <>{projectData[0].info}</>
+              <GithubButton onClick={(e) => {window.open(`${projectData[0].repo}`)}}><GitStyle/></GithubButton>
+            </GridText>
+            <GridImage item
+            position='relative' xs={12} sm={12} md={6}
+            height={{xs: '50%'}} image={projectData[1].img}/>
+            <GridText container item
+            position='relative' xs={12} sm={12} md={6}
+            height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>
+              <>{projectData[1].title}</><br></br>
+              <>{projectData[1].info}</>
+              <GithubButton onClick={(e) => {window.open(`${projectData[1].repo}`)}}><GitStyle/></GithubButton>
+            </GridText>
+            <GridImage item
+            position='relative' xs={12} sm={12} md={6}
+            height={{xs: '50%'}} image={projectData[2].img}/>
+            <GridText container item
+            position='relative' xs={12} sm={12} md={6}
+            height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>
+              <>{projectData[2].title}</><br></br>
+              <>{projectData[2].info}</>
+              <GithubButton onClick={(e) => {window.open(`${projectData[2].repo}`)}}><GitStyle/></GithubButton>
+            </GridText>
           </Grid>
         </GridContainer>
       </ProjectsBox>
@@ -61,14 +85,17 @@ const ProjectsTitle = styled.div`
 const GridContainer = styled.div`
   display: flex;
   height: 100%;
-  border: solid white;
   overflow: scrollY;
   padding: 5px;
 `;
 
 const GridText = styled(Grid)`
   display: flex;
-  padding: 1%;
+  padding: 5px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
 `;
 
 const GridImage = styled(Grid)`
@@ -76,9 +103,42 @@ const GridImage = styled(Grid)`
   background-image: url(${props => props.image});
   background-size: cover;
   background-position: center;
-  max-width:100%;
-  max-height:100%;
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 5px;
+  box-shadow: 1px 1px 3px;
 `;
+
+const GithubButton = styled.div`
+  border: solid white;
+  height: 10%;
+  width: 50%;
+  margin-top: auto;
+  padding: 5px;
+  border-radius: 5px;
+  border-color: #fffcf2;
+  :hover {
+    border-color: #eb5e28;
+    box-shadow: 0 0 3px #eb5e28;
+    transition: all .2s ease-in-out;
+    cursor: pointer;
+  }
+`;
+
+const GitStyle = styled(GitHubIcon)`
+  color: #fffcf2;
+  margin: auto;
+  border: solid transparent;
+  border-radius: 100%;
+  ${GithubButton}:hover & {
+    color: #eb5e28;
+    transition: all .2s ease-in-out;
+  }
+`;
+
+const GridTextTitle = styled.p`
+  font-weight: bold;
+`
 // #fffcf2 floral white,
 //    pop out, headers and titles
 // #ccc5b9 pale silver,
--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -4,19 +4,19 @@ const dbz = require('../media/dbz.PNG')
 
 const projectData = [
   {
-    title: 'fec',
+    title: 'Project Atelier',
     info: `Delivered a fully functional web application that met the specifications and requirements outlined by the project stakeholders`,
     img: fec,
     repo: 'https://github.com/TeamSnuggie/FEC',
   },
   {
-    title: 'sdc',
+    title: 'Soulful Backend',
     info: `Replaced legacy server/ API for an E-commerce web application with a MVC design pattern and PostgreSQL database`,
     img: sdc,
     repo: 'https://github.com/mrdooby/muppets',
   },
   {
-    title: 'blue ocean',
+    title: 'Vegetation Station',
     info: `Launched a mobile iOS application that allows users to trade plants with other plant lovers within a 150-mile radius`,
     img: dbz,
     repo: 'https://github.com/Blue-Ocean-DBZ/vegetationstation'
