8a02e42e66708cca5f9afdf4919fd3bc140cc2e5
mrdooby <mrdooby@DESKTOP-OC2PQIT.localdomain>
2022-08-24 20:40:00 -0700

    feat: projects grid scales beautifully to all screen types

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,13 +16,15 @@ const Projects = () => {
         <GridContainer>
           <Grid container spacing={0}
           border='solid pink'
-          direction='column'>
-            <GridImage item border='solid red' position='relative' md={6}></GridImage>
-            <GridText item border='solid red' position='relative' md={6}>value1</GridText>
-            <GridImage item border='solid blue' position='relative' md={6}></GridImage>
-            <GridText item border='solid blue' position='relative' md={6}>value2</GridText>
-            <GridImage item border='solid yellow' position='relative' md={6}></GridImage>
-            <GridText item border='solid yellow' position='relative' md={6}>value3</GridText>
+          direction={{xs: 'row', sm: 'row', md: 'column'}}
+          overflow='auto'
+          >
+            <GridImage item border='solid red' position='relative' xs={12} sm={12} md={6} height={{xs: '50%'}} image={projectData[0].img}></GridImage>
+            <GridText item border='solid red' position='relative' xs={12} sm={12} md={6} height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>{projectData[0].info}</GridText>
+            <GridImage item border='solid blue' position='relative' xs={12} sm={12} md={6} height={{xs: '50%'}} image={projectData[1].img}></GridImage>
+            <GridText item border='solid blue' position='relative' xs={12} sm={12} md={6} height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>{projectData[1].info}</GridText>
+            <GridImage item border='solid yellow' position='relative' xs={12} sm={12} md={6} height={{xs: '50%'}} image={projectData[2].img}></GridImage>
+            <GridText item border='solid yellow' position='relative' xs={12} sm={12} md={6} height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>{projectData[2].info}</GridText>
           </Grid>
         </GridContainer>
       </ProjectsBox>
@@ -60,14 +62,22 @@ const GridContainer = styled.div`
   display: flex;
   height: 100%;
   border: solid white;
-  overflow: auto;
+  overflow: scrollY;
   padding: 5px;
 `;
 
 const GridText = styled(Grid)`
+  display: flex;
+  padding: 1%;
 `;
 
 const GridImage = styled(Grid)`
+  display: flex;
+  background-image: url(${props => props.image});
+  background-size: cover;
+  background-position: center;
+  max-width:100%;
+  max-height:100%;
 `;
 // #fffcf2 floral white,
 //    pop out, headers and titles
