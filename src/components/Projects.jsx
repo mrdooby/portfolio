ff8013be0ed7dd39f54f8617c6acf2a1cff3835c
mrdooby <mrdooby@DESKTOP-OC2PQIT.localdomain>
2022-08-26 09:04:00 -0700

    feat: projects spacing and boxes full screen

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,45 +9,35 @@ const Projects = () => {
   return (
     <ProjectsContainer>
       <ProjectsTitle>Projects</ProjectsTitle>
-        <GridContainer>
-          <Grid container spacing={0}
-          direction={{xs: 'row', sm: 'row', md: 'column'}}
-          overflow='auto'
-          >
-            <GridImage item
-            position='relative' xs={12} sm={12} md={6} height={{xs: '50%'}}
-            >
-              <Image/>
-            </GridImage>
-            <GridText container item
-            position='relative' xs={12} sm={12} md={6}
-            height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>
-              <>{projectData[0].title}</><br></br>
-              <>{projectData[0].info}</>
-              <GithubButton onClick={(e) => {window.open(`${projectData[0].repo}`)}}><GitStyle/></GithubButton>
-            </GridText>
-            <GridImage item
-            position='relative' xs={12} sm={12} md={6}
-            height={{xs: '50%'}} image={projectData[1].img}/>
-            <GridText container item
-            position='relative' xs={12} sm={12} md={6}
-            height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>
-              <>{projectData[1].title}</><br></br>
-              <>{projectData[1].info}</>
-              <GithubButton onClick={(e) => {window.open(`${projectData[1].repo}`)}}><GitStyle/></GithubButton>
-            </GridText>
-            <GridImage item
-            position='relative' xs={12} sm={12} md={6}
-            height={{xs: '50%'}} image={projectData[2].img}/>
-            <GridText container item
-            position='relative' xs={12} sm={12} md={6}
-            height='50%' width={{ xs: '100%', sm: '100%', md: '33.33%'}}>
-              <>{projectData[2].title}</><br></br>
-              <>{projectData[2].info}</>
-              <GithubButton onClick={(e) => {window.open(`${projectData[2].repo}`)}}><GitStyle/></GithubButton>
-            </GridText>
+      <Grid container spacing={1} position='relative'
+      marginBottom='auto' height={{md: '100%'}}
+      border='solid blue' direction='row' paddingLeft='12px'
+      >
+        <Grid container direction='column' height='100%' width='calc(100%/3)'>
+          <Grid item md={6} position='relative' padding='20px'>
+            <ProjectsPhoto image={projectData[0].img}></ProjectsPhoto>
           </Grid>
-        </GridContainer>
+          <Grid item md={6} padding='20px'>
+            <ProjectsText>{projectData[0].title}<br/>{projectData[0].info}</ProjectsText>
+          </Grid>
+        </Grid>
+        <Grid container direction='column' height='100%' width='calc(100%/3)'>
+        <Grid item md={6} position='relative' padding='20px'>
+            <ProjectsPhoto image={projectData[1].img}></ProjectsPhoto>
+          </Grid>
+          <Grid item md={6} padding='20px'>
+          <ProjectsText>{projectData[1].title}<br/>{projectData[1].info}</ProjectsText>
+          </Grid>
+        </Grid>
+        <Grid container direction='column' height='100%' width='calc(100%/3)'>
+          <Grid item md={6} position='relative' padding='20px'>
+            <ProjectsPhoto image={projectData[2].img}></ProjectsPhoto>
+          </Grid>
+          <Grid item md={6} padding='20px'>
+          <ProjectsText>{projectData[2].title}<br/>{projectData[2].info}</ProjectsText>
+          </Grid>
+        </Grid>
+    </Grid>
     </ProjectsContainer>
   )
 }
@@ -72,71 +62,24 @@ const ProjectsTitle = styled.div`
   color: #fffcf2;
 `;
 
-const GridContainer = styled.div`
-  display: flex;
-  height: 100%;
-  overflow: scrollY;
-  padding: 5px;
-`;
-
-const GridText = styled(Grid)`
-  display: flex;
-  padding: 5px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 20px;
-`;
-
-const GridImage = styled(Grid)`
-  display: flex;
-  background-image: url(${props => props.image});
-  background-size: cover;
-  background-position: center;
-  max-width: 100%;
-  max-height: 100%;
-  border-radius: 5px;
-  box-shadow: 1px 1px 3px;
-`;
-
-const Image = styled.div`
+const ProjectsPhoto = styled.div`
   display: flex;
   border: solid yellow;
-  background-image: url('https://i.kym-cdn.com/photos/images/newsfeed/001/464/818/f2a.jpg');
+  background-image: url(${props => props.image});
   height: 100%;
+  background-size: cover;
 `;
 
-const GithubButton = styled.div`
-  border: solid white;
-  height: 10%;
-  width: 30%;
-  margin-top: auto;
-  padding: 5px;
-  border-radius: 5px;
-  border-color: #fffcf2;
-  text-align: center;
-  :hover {
-    border-color: #eb5e28;
-    box-shadow: 0 0 3px #eb5e28;
-    transition: all .2s ease-in-out;
-    cursor: pointer;
-  }
+const ProjectsText = styled.div`
+  display: flex;
+  border: solid green;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  padding-left: 100px;
+  padding-right: 100px;
+  font-size: 1vw;
 `;
-
-const GitStyle = styled(GitHubIcon)`
-  color: #fffcf2;
-  margin: auto;
-  border: solid transparent;
-  border-radius: 100%;
-  ${GithubButton}:hover & {
-    color: #eb5e28;
-    transition: all .2s ease-in-out;
-  }
-`;
-
-const GridTextTitle = styled.p`
-  font-weight: bold;
-`
 // #fffcf2 floral white,
 //    pop out, headers and titles
 // #ccc5b9 pale silver,
