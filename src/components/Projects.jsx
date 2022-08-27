fb101fd502eae35e2ac4da479d0aa4c460448ea8
mrdooby <mrdooby@DESKTOP-OC2PQIT.localdomain>
2022-08-26 18:52:17 -0700

    feat: misc changes, added button

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Grid from '@mui/material/Grid';
 import { projectData } from '../data/ProjectsData.js';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import Card from '@mui/material/Card';
 
 const Projects = () => {
 
@@ -18,7 +19,10 @@ const Projects = () => {
             <ProjectsPhoto image={projectData[0].img}></ProjectsPhoto>
           </Grid>
           <Grid item md={6} padding='20px'>
-            <ProjectsText>{projectData[0].title}<br/>{projectData[0].info}</ProjectsText>
+            <ProjectsText>
+              {projectData[0].title}<br/>{projectData[0].info}
+              <GithubButton onClick={(e) => {window.open(`${projectData[0].repo}`)}}><GitStyle/></GithubButton>
+            </ProjectsText>
           </Grid>
         </Grid>
         <Grid container direction='column' height='100%' width='calc(100%/3)'>
@@ -26,7 +30,10 @@ const Projects = () => {
             <ProjectsPhoto image={projectData[1].img}></ProjectsPhoto>
           </Grid>
           <Grid item md={6} padding='20px'>
-          <ProjectsText>{projectData[1].title}<br/>{projectData[1].info}</ProjectsText>
+          <ProjectsText>
+            {projectData[1].title}<br/>{projectData[1].info}
+            <GithubButton onClick={(e) => {window.open(`${projectData[0].repo}`)}}><GitStyle/></GithubButton>
+          </ProjectsText>
           </Grid>
         </Grid>
         <Grid container direction='column' height='100%' width='calc(100%/3)'>
@@ -34,7 +41,10 @@ const Projects = () => {
             <ProjectsPhoto image={projectData[2].img}></ProjectsPhoto>
           </Grid>
           <Grid item md={6} padding='20px'>
-          <ProjectsText>{projectData[2].title}<br/>{projectData[2].info}</ProjectsText>
+          <ProjectsText>
+            {projectData[2].title}<br/>{projectData[2].info}
+            <GithubButton onClick={(e) => {window.open(`${projectData[0].repo}`)}}><GitStyle/></GithubButton>
+            </ProjectsText>
           </Grid>
         </Grid>
     </Grid>
@@ -79,6 +89,34 @@ const ProjectsText = styled.div`
   padding-left: 100px;
   padding-right: 100px;
   font-size: 1vw;
+  flex-direction: column;
+`;
+
+const GithubButton = styled.div`
+  border: solid white;
+  height: 10%;
+  width: 50%;
+  margin-top: auto;
+  padding: 5px;
+  border-radius: 5px;
+  border-color: #fffcf2;
+  :hover {
+    border-color: #eb5e28;
+    box-shadow: 0 0 3px #eb5e28;
+    transition: all .2s ease-in-out;
+    cursor: pointer;
+  }
+`;
+
+const GitStyle = styled(GitHubIcon)`
+  color: #fffcf2;
+  margin: auto;
+  border: solid transparent;
+  border-radius: 100%;
+  ${GithubButton}:hover & {
+    color: #eb5e28;
+    transition: all .2s ease-in-out;
+  }
 `;
 // #fffcf2 floral white,
 //    pop out, headers and titles
