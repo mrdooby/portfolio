33caa302e474c36a4099914316cadfb2fd9e4946
mrdooby <mrdooby@DESKTOP-OC2PQIT.localdomain>
2022-09-02 10:52:20 -0700

    feat: projects text imported

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, Suspense } from 'react';
 import styled from 'styled-components';
 import Grid from '@mui/material/Grid';
+import { projectData } from '../data/ProjectsData.js';
 
 const Projects = () => {
   return (
@@ -11,28 +12,34 @@ const Projects = () => {
          height='45vh'
         >
           <Grid item md={6} borderRight='solid purple'>
-            Photo 1
+            <ProjectsPhoto image={projectData[0].img}/>
           </Grid>
-          <Grid item md={6}>
-            Text 1
+          <Grid item md={6} padding='1vw' display='flex' direction='column'>
+            <ProjectsTitle>{projectData[0].title}</ProjectsTitle>
+            <ProjectsText>{projectData[0].info}</ProjectsText>
+            <ProjectsMembers>{projectData[0].members}</ProjectsMembers>
           </Grid>
         </Grid>
-        <Grid container item border='solid pink' marginBottom='2vw' width='90%'
+      <Grid container item border='solid pink' marginBottom='2vw' width='90%'
         marginLeft='auto' height='45vh'
         >
-        <Grid item md={6} borderRight='solid purple'>
-            Photo 2
+          <Grid item md={6} borderRight='solid purple' padding='1vw' display='flex' direction='column'>
+            <ProjectsTitle>{projectData[1].title}</ProjectsTitle>
+            <ProjectsText>{projectData[1].info}</ProjectsText>
+            <ProjectsMembers>{projectData[1].members}</ProjectsMembers>
           </Grid>
           <Grid item md={6}>
-            Text 2
+            Photo 2
           </Grid>
         </Grid>
         <Grid container item border='solid yellow' width='90%' height='45vh'>
         <Grid item md={6} borderRight='solid purple'>
             Photo 3
           </Grid>
-          <Grid item md={6}>
-            Text 3
+          <Grid item md={6} padding='1vw' display='flex' direction='column'>
+            <ProjectsTitle>{projectData[2].title}</ProjectsTitle>
+            <ProjectsText>{projectData[2].info}</ProjectsText>
+            <ProjectsMembers>{projectData[2].members}</ProjectsMembers>
           </Grid>
         </Grid>
       </Grid>
@@ -47,3 +54,36 @@ const ProjectsContainer = styled.section`
   display: flex;
   border: solid black;
 `;
+
+const ProjectsTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: max(20px, 2vw);
+  font-weight: bold;
+`
+
+const ProjectsText = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: max(10px, 1vw);
+  font-weight: bold;
+  white-space: break-spaces;
+`;
+
+const ProjectsMembers = styled.div`
+  display: flex;
+  margin-top: auto;
+  margin-left: auto;
+  color: #403d39;
+`;
+
+// #fffcf2 floral white,
+//    pop out, headers and titles
+// #ccc5b9 pale silver,
+//    normal text
+// #403d39 black olive,
+//    box around text
+// #252422 eerie black,
+//    background
+// #eb5e28 flame
+//    hightlight around buttons s
--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -8,18 +8,21 @@ const projectData = [
     info: `Delivered a fully functional web application that met the specifications and requirements outlined by the project stakeholders\n• Refactored React components by removing repetitive code and maximizing the reusability of components, improving the Lighthouse performance score by 12 points\n• Worked on an agile team of four engineers and used Git and Trello to keep organized on a single codebase\n• Implemented unit and end-to-end testing using Jest/ React Testing Library framework`,
     img: fec,
     repo: 'https://github.com/TeamSnuggie/FEC',
+    members: 'Aaron Pan, Clayton Chin, David Arredondo, Jasper Bucad'
   },
   {
     title: 'Soulful Backend',
     info: `Replaced legacy server/ API for an E-commerce web application with a MVC design pattern and PostgreSQL database\n• Used the ETL process on millions of rows of data and formatted the queries to return the same formatted data that needs to be received by the front end\n• Local stress tested server with K6 and stress tested deployed server on Loader.io\n• Horizontally scaled the application with multiple Amazon EC2 instances and added a NGINX load balancer to handle over 2000 RPS with a less than 1% error rate`,
     img: sdc,
     repo: 'https://github.com/mrdooby/muppets',
+    members: 'Aaron Pan, Clayton Chin, Frederick Pascua'
   },
   {
     title: 'Vegetation Station',
-    info: `Launched a mobile iOS application that allows users to trade plants with other plant lovers within a 150-mile radius\n• Collaborated with a team of nine engineers to create the product starting from wireframing to presenting live working demos to the client\n• Created and connected React Native widgets and components in line with Test Driven Development best practices\n• Designed the profile page and chat messages components for our app\n• Connected our Express server to our PostgreSQL database to create schemas and write queries to feed relevant data into our application`,
+    info: `Launched a mobile iOS application that allows users to trade plants with other plant lovers within a 150-mile radius\n• Collaborated with a team of nine engineers to create the product starting from wireframing to presenting live working demos to the client\n• Created and connected React Native widgets and components in line with Test Driven Development best practices\n• Designed the profile page and chat messages components for our app\n`,
     img: dbz,
-    repo: 'https://github.com/Blue-Ocean-DBZ/vegetationstation'
+    repo: 'https://github.com/Blue-Ocean-DBZ/vegetationstation',
+    members: 'Brandon Hsu, Carson Liu, Clayton Chin, David Arredondo, Gian Franco Lazaro, Jonathan Oh, Kevin Ha, Shannon Murphy, Theresa Tran'
   }
 ];
 
